b3430b311479d3374559015a91a6407c69e0cda3
CarlR0P <davidrp123@hotmail.com>
2023-09-05 13:51:26 -0500

    Yo adivino un número

---
+++ taller-2-java/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import  java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int intrandomNumber,n,min=0,max=100, contador = 0;
+
+        Random random = new Random();
+        intrandomNumber=random.nextInt(100)+1;
+
+        do{
+            System.out.println("Ingrese su jugada entre "+min+" y "+max);
+            n=sc.nextInt();
+
+            contador++;
+
+            if(n<=min||n>=max) {
+                System.out.println("PERDIO");
+                break;
+            }
+            if(n==intrandomNumber)
+                System.out.println("Felicitaciones ");
+            else {
+                if(n>intrandomNumber)
+                {
+                    max=n;
+                    System.out.println("EL SECRETO ES MENOR");
+                }
+                else
+                {
+                    min=n;
+                    System.out.println("EL SECRETO ES MAYOR");
+                }
+
+            }
+            System.out.println("intentos: "+contador);
+        }while(n!=intrandomNumber&&n>=min&&n<=max);
+        if(n==intrandomNumber)
+            System.out.println("Felicitaciones ");
+    }
+}
