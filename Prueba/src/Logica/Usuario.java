1505e3399b9f87007af14771cba902c1a1bc2dbc
CarlR0P <davidrp123@hotmail.com>
2023-11-24 01:24:51 -0500

    Se sube segundo avance del proyecto Final transformado a POO(Programacion orientada a objetos), se corrigen varias opciones de los roles que no estaban funcionando correctamente, se reubicaron los diccionarios de horarios y mensajes y se modificaron los respectivos constructores y getters/setters. Se borraron algunas librerias en clases que no las necesitaban.

--- Prueba/src/Logica/Usuario.java
+++ Prueba/src/Logica/Usuario.java
@@ -1,8 +1,6 @@
 package Logica;
 
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Usuario {
@@ -14,8 +12,6 @@ public class Usuario {
     private String numCedula;
     private String mensaje;
     private String horario;
-    private HashMap<String, List<String>> horarios;
-    private HashMap<String, List<String>> mensajes;
 
     public Usuario(String codigo, String nombre, String rol, String tipoDoc, String numCedula, String horario, SistemaRegistro sistema) {
         this.codigo = codigo;
@@ -23,12 +19,9 @@ public class Usuario {
         this.rol = rol;
         this.tipoDoc = tipoDoc;
         this.numCedula = numCedula;
-        this.mensajes = new HashMap<>();
         this.mensaje = "";
-        this.horarios = new HashMap<>();
         this.horario = "";
         this.sistema = sistema;
-
     }
 
     public String getCodigo() {
@@ -79,12 +72,12 @@ public class Usuario {
         this.mensaje = mensaje;
     }
 
-    public HashMap<String, List<String>> getHorarios() {
-        return horarios;
+    public String getHorario() {
+        return horario;
     }
 
-    public void setHorarios(HashMap<String, List<String>> horarios) {
-        this.horarios = horarios;
+    public void setHorario(String horario) {
+        this.horario = horario;
     }
 
     public void menuOpciones() {
@@ -98,8 +91,9 @@ public class Usuario {
         System.out.println("Número de Cédula: " + sistema.getNumCedulas().get(codigo));
     }
     public void revisarMensajes() {
-        if (mensajes.containsKey(codigo)) {
-            List<String> listaMensajes = mensajes.get(codigo);
+
+        List<String> listaMensajes = sistema.obtenerMensajes(codigo);
+        if (!listaMensajes.isEmpty()) {
             System.out.println("Tiene los siguientes mensajes:");
             for (String mensaje : listaMensajes) {
                 System.out.println("- " + mensaje);
@@ -111,19 +105,16 @@ public class Usuario {
     public void enviarMensajes() {
         Scanner lector = new Scanner(System.in);
         System.out.print("Ingrese el código del destinatario: ");
-        String codigoDestinatario  = lector.nextLine();
-        if (sistema.existeUsuario(codigoDestinatario )) {
-            nombre = sistema.getNombresLaboral().get(codigo);
+        String codigoDestinatario = lector.nextLine();
+
+        if (sistema.existeUsuario(codigoDestinatario)) {
             System.out.print("Ingrese el mensaje: ");
-            mensaje = lector.nextLine();
-            String mensajeCompleto = "De: " + nombre + ". Mensaje: " + mensaje;
-            if (mensajes.containsKey(codigoDestinatario)) {
-                mensajes.get(codigoDestinatario).add(mensajeCompleto);
-            } else {
-                List<String> listaMensajes = new ArrayList<>();
-                listaMensajes.add(mensajeCompleto);
-                mensajes.put(codigoDestinatario, listaMensajes);
-            }
+            String mensaje = lector.nextLine();
+            String nombreRemitente = sistema.getNombresLaboral().get(codigo);
+            String mensajeCompleto = "De: " + nombreRemitente + ". Mensaje: " + mensaje;
+
+            sistema.enviarMensajes(codigo, codigoDestinatario, mensajeCompleto);
+
             System.out.println("Mensaje enviado correctamente.");
         } else {
             System.out.println("No existe ningún usuario con el código " + codigoDestinatario);
